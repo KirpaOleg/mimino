c9775f4040e364c454728fd395482a87fe283a8c
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2022-12-04 20:26:12 +0000

    Deploying to gh-pages from @ GregTerekhov/mimino-project@81c5781432935604ef84ffe76db0db1f57968c64 🚀

--- hotel.8143af37.js
+++
@@ -1,2 +0,0 @@
-var swiper=new Swiper(".mySwiper",{slidesPerView:2,initialSlide:4,spaceBetween:16,centeredSlides:!0,freeMode:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},breakpoints:{320:{slidesPerView:2,spaceBetween:16},1024:{slidesPerView:2,spaceBetween:40}}});
-//# sourceMappingURL=hotel.8143af37.js.map
