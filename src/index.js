e98aab8e8e7f51e2e6555a3b92f3eb152b95f400
OleksandrYaromenko <oleksandryaromenko21@gmail.com>
2022-12-06 12:10:31 +0100

    майже закінчив

---
+++ src/index.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".mySwiper", {
+  // Optional parameters
+  slidesPerView: "auto",
+    spaceBetween: 30,
+    loopAdditionalSlides: 0,
+    rewind: true,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
